d586c0c51702d70b81c499954f13ed65c9849b16
2P acquisition <geffenlab@gmail.com>
2017-03-15 12:26:54 -0400

    added in the text file code and changed saving back to folder numbers

--- generalCode/makeInitialStacksScript.m
+++ generalCode/makeInitialStacksScript.m
@@ -1,12 +1,28 @@
 clear
 
 % Enter info here
-mouse = 'K048';
-dateOfRecording = '20170309';
+mouse = 'K019';
+dateOfRecording = '20160916';
 dataFolder = [dateOfRecording mouse];
 
+% open and/or make excel file for mouse
+dataAnalysisComp = '\\DESKTOP-GK8OVIP\data\';
+f = dir([dataAnalysisComp mouse '\']);
+csvExist = any(~cellfun(@isempty,strfind({f.name},'.txt')));
+csvName = [dataAnalysisComp '\' mouse '\' mouse '_experiments.txt'];
+if ~csvExist
+    headers = {'date','recording name','folder number'};   
+    fid = fopen(csvName,'a');
+    for ii=1:length(headers)
+        fprintf(fid,'%s\t',headers{ii})
+    end
+else
+    fid = fopen(csvName,'a');
+end
+
+
 %% make stacks
-dataPath = ['F:\' mouse '\' dataFolder '\'];
+dataPath = ['E:\' mouse '\' dataFolder '\'];
 folders = dir(dataPath);
 folders = folders([folders.isdir]); 
 folders(strcmp({folders.name},'.'))=[]; % get rid of stupid windows directories
@@ -19,8 +35,11 @@ for ii =1:length(folders)
     tiffs = dir([dataPath folders(ii).name '\*.tif']);    
     if length(tiffs)>1
         saveFileName =  folders(ii).name;
-        saveFolder = ['G:\TEMP\' dataFolder '_tifStacks\' num2str(ind) '\'];
+%         dash = strfind(folders(ii).name,'-');
+%         fne = folders(ii).name(dash(1)+1:end);
+        saveFolder = ['G:\TEMP\' dataFolder '_tifStacks\' sprintf('%02d',ind) '\'];
         tiffStackMaker([dataPath folders(ii).name],'.ome',saveFileName,saveFolder)
+        fprintf(fid,'\n %s\t%s\t%02d',dateOfRecording,saveFileName,ind)
         ind = ind+1;
       otherData = dir([dataPath folders(ii).name]);
       otherData = otherData(~[otherData.isdir]);
@@ -36,8 +55,18 @@ for ii =1:length(folders)
   
     toc
 end
-toc
-    
+
+for ii =1:length(folders)
+    disp(['Folder ' num2str(ii) '/' num2str(length(folders))])
+    tiffs = dir([dataPath folders(ii).name '\*.tif']);    
+    if length(tiffs)==1
+        saveFileName =  folders(ii).name;
+        saveFolder = ['G:\TEMP\' dataFolder '_tifStacks\singleImages\'];
+         copyfile([dataPath folders(ii).name], saveFolder)
+        
+    end
+end
+    fclose(fid)
 %% Now transfer the files to the analysis computer...
 if ~isdir(['\\DESKTOP-GK8OVIP\data\' mouse '\' dataFolder '_tifStacks\'])
     mkdir(['\\DESKTOP-GK8OVIP\data\' mouse '\' dataFolder '_tifStacks\']);
